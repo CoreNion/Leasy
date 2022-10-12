907c7f00d02024dc277ff1250e86bc91a7aa9bad
CoreNion <coreinion@gmail.com>
2022-10-12 14:03:20 +0900

    [UI] Dynamic Colorに対応

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,25 +8,27 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return DynamicColorBuilder(
+        builder: ((lightDynamic, darkDynamic) => MaterialApp(
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                  colorScheme: lightDynamic != null
+                      ? lightDynamic.harmonized()
+                      : ColorScheme.fromSeed(
+                          seedColor: Colors.blue, brightness: Brightness.light),
+                  useMaterial3: true),
+              darkTheme: ThemeData(
+                  colorScheme: darkDynamic != null
+                      ? darkDynamic.harmonized()
+                      : ColorScheme.fromSeed(
+                          seedColor: Colors.blue,
+                          brightness: Brightness.dark,
+                        ),
+                  useMaterial3: true),
+              home: const MyHomePage(title: 'Flutter Demo Home Page'),
+            )));
   }
 }
 
