38dceb21f62b020a600cbde4fa11671c16c09151
CoreNion <coreinion@gmail.com>
2022-10-14 15:36:13 +0900

    [top] 問題一覧のListを整備
    レスポンシブ対応
    
    #3

--- lib/pages/top.dart
+++ lib/pages/top.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:responsive_grid_list/responsive_grid_list.dart';
 
 class TopPage extends StatefulHookConsumerWidget {
   const TopPage({super.key});
@@ -11,6 +12,31 @@ class TopPage extends StatefulHookConsumerWidget {
 class _TopPageState extends ConsumerState<TopPage> {
   @override
   Widget build(BuildContext context) {
-    return const Text("top");
+    List<Widget> subjectList = <Widget>[];
+    for (var i = 0; i < 13; i++) {
+      subjectList.add(
+        Container(
+          height: 150,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border:
+                Border.all(color: Theme.of(context).colorScheme.onBackground),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Text(
+            i.toString(),
+          ),
+        ),
+      );
+    }
+
+    return ResponsiveGridList(
+      minItemWidth: 250,
+      horizontalGridMargin: 20,
+      horizontalGridSpacing: 30,
+      verticalGridSpacing: 30,
+      verticalGridMargin: 20,
+      children: subjectList,
+    );
   }
 }
