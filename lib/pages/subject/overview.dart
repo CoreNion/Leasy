e867ec23bc23a03089018a081c617621ff7ebc9b
CoreNion <coreinion@gmail.com>
2022-11-04 14:58:19 +0900

    [overview] 基礎の組み立て

--- lib/pages/subject/overview.dart
+++ lib/pages/subject/overview.dart
@@ -13,6 +13,33 @@ class SubjectOverview extends StatefulHookConsumerWidget {
 class _SubjectOverviewState extends ConsumerState<SubjectOverview> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text(widget.title)));
+    return Scaffold(
+        appBar: AppBar(title: Text(widget.title)),
+        body: Center(
+            child: Padding(
+          padding: const EdgeInsets.all(7.0),
+          child: Column(children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.all(7.0),
+                    child: ElevatedButton(
+                      onPressed: null,
+                      child: Text("続きから学習を開始する"),
+                    )),
+                Padding(
+                    padding: const EdgeInsets.all(7.0),
+                    child: ElevatedButton(
+                        onPressed: null, child: Text("テストを開始する"))),
+              ],
+            ),
+            const Divider(),
+            Text(
+              "セクション数:${1} 単語数:${1}",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+            )
+          ]),
+        )));
   }
 }
