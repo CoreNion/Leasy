f0cf8efd24da267523bd83e15791cabfb623d1d5
CoreNion <coreinion@gmail.com>
2022-11-07 13:43:21 +0900

    [overview] AppBarにセクション作成ボタンを追加

--- lib/pages/subject/overview.dart
+++ lib/pages/subject/overview.dart
@@ -11,31 +11,74 @@ class SubjectOverview extends StatefulHookConsumerWidget {
 }
 
 class _SubjectOverviewState extends ConsumerState<SubjectOverview> {
+  final _formKey = GlobalKey<FormState>();
+  String _title = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text(widget.title)),
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: <Widget>[
+            IconButton(
+                onPressed: (() => showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                          title: const Text("セクションを作成"),
+                          actions: <Widget>[
+                            TextButton(
+                                onPressed: (() => Navigator.pop(context)),
+                                child: const Text("キャンセル")),
+                            TextButton(
+                                onPressed: (() {
+                                  if (_formKey.currentState!.validate()) {
+                                    print(_title);
+                                    Navigator.pop(context);
+                                  }
+                                }),
+                                child: const Text("決定")),
+                          ],
+                          content: Form(
+                              key: _formKey,
+                              child: TextFormField(
+                                decoration: const InputDecoration(
+                                    labelText: "セクション名",
+                                    icon: Icon(Icons.book),
+                                    hintText: "セクション名を入力"),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return "セクション名を入力してください";
+                                  } else {
+                                    _title = value;
+                                    return null;
+                                  }
+                                },
+                              ))),
+                    )),
+                icon: const Icon(Icons.add))
+          ],
+        ),
         body: Center(
             child: Padding(
           padding: const EdgeInsets.all(7.0),
           child: Column(children: <Widget>[
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
+              children: const <Widget>[
                 Padding(
-                    padding: const EdgeInsets.all(7.0),
+                    padding: EdgeInsets.all(7.0),
                     child: ElevatedButton(
                       onPressed: null,
                       child: Text("続きから学習を開始する"),
                     )),
                 Padding(
-                    padding: const EdgeInsets.all(7.0),
+                    padding: EdgeInsets.all(7.0),
                     child: ElevatedButton(
                         onPressed: null, child: Text("テストを開始する"))),
               ],
             ),
             const Divider(),
-            Text(
+            const Text(
               "セクション数:${1} 単語数:${1}",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
             )
