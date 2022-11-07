c5b37fd61e0d9289d9ace159c709703c81443bb6
CoreNion <coreinion@gmail.com>
2022-11-07 15:54:10 +0900

    [overview] セクションリストを整備

--- lib/pages/subject/overview.dart
+++ lib/pages/subject/overview.dart
@@ -13,7 +13,20 @@ class SubjectOverview extends StatefulHookConsumerWidget {
 
 class _SubjectOverviewState extends ConsumerState<SubjectOverview> {
   final _formKey = GlobalKey<FormState>();
-  String _sectionTitle = "";
+  String _createdSectionTitle = "";
+  final List<String> _sectionList = <String>[];
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 保存されているセクションをリストに追加
+    DataBaseHelper.getSectionTitles(widget.title).then((titles) {
+      for (var title in titles) {
+        setState(() => _sectionList.add(title));
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,9 +47,10 @@ class _SubjectOverviewState extends ConsumerState<SubjectOverview> {
                                 onPressed: (() async {
                                   if (_formKey.currentState!.validate()) {
                                     await DataBaseHelper.createSection(
-                                        widget.title, _sectionTitle);
-                                    print(await DataBaseHelper.getSectionTitles(
-                                        widget.title));
+                                        widget.title, _createdSectionTitle);
+                                    setState(() {
+                                      _sectionList.add(_createdSectionTitle);
+                                    });
 
                                     Navigator.pop(context);
                                   }
@@ -54,7 +68,7 @@ class _SubjectOverviewState extends ConsumerState<SubjectOverview> {
                                   if (value == null || value.isEmpty) {
                                     return "セクション名を入力してください";
                                   } else {
-                                    _sectionTitle = value;
+                                    _createdSectionTitle = value;
                                     return null;
                                   }
                                 },
@@ -63,31 +77,78 @@ class _SubjectOverviewState extends ConsumerState<SubjectOverview> {
                 icon: const Icon(Icons.add))
           ],
         ),
-        body: Center(
-            child: Padding(
-          padding: const EdgeInsets.all(7.0),
-          child: Column(children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const <Widget>[
-                Padding(
-                    padding: EdgeInsets.all(7.0),
-                    child: ElevatedButton(
-                      onPressed: null,
-                      child: Text("続きから学習を開始する"),
-                    )),
-                Padding(
-                    padding: EdgeInsets.all(7.0),
-                    child: ElevatedButton(
-                        onPressed: null, child: Text("テストを開始する"))),
-              ],
-            ),
-            const Divider(),
-            const Text(
-              "セクション数:${1} 単語数:${1}",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-            )
-          ]),
-        )));
+        body: Padding(
+            padding: const EdgeInsets.all(7.0),
+            child: SingleChildScrollView(
+              child: Column(children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Padding(
+                        padding: EdgeInsets.all(7.0),
+                        child: ElevatedButton(
+                          onPressed: null,
+                          child: Text("続きから学習を開始する"),
+                        )),
+                    Padding(
+                        padding: EdgeInsets.all(7.0),
+                        child: ElevatedButton(
+                            onPressed: null, child: Text("テストを開始する"))),
+                  ],
+                ),
+                Text(
+                  "セクション数:${_sectionList.length}",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+                const Divider(),
+                ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: _sectionList.length,
+                    itemBuilder: ((context, index) => Dismissible(
+                        key: Key(_sectionList[index]),
+                        onDismissed: (direction) {
+                          // wip
+                        },
+                        confirmDismiss: (direction) async {
+                          return await showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text('${_sectionList[index]}を削除しますか？'),
+                                content: const Text('この操作は取り消せません。'),
+                                actions: [
+                                  SimpleDialogOption(
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(true),
+                                    child: const Text('はい'),
+                                  ),
+                                  SimpleDialogOption(
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(false),
+                                    child: const Text('いいえ'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        background: Container(
+                          color: Colors.red,
+                          padding: const EdgeInsets.only(left: 10, right: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: const <Widget>[
+                              Icon(Icons.delete),
+                              Icon(Icons.delete)
+                            ],
+                          ),
+                        ),
+                        child: ListTile(
+                          title: Text(_sectionList[index]),
+                        )))),
+              ]),
+            )));
   }
 }
