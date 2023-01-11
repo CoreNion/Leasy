6265d7f8130d20374feaaf5b893b740dfcc38b0c
CoreNion <coreinion@gmail.com>
2023-01-11 15:24:58 +0900

    [setting] `study.db`を削除するボタンを作成

--- lib/pages/setting.dart
+++ lib/pages/setting.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:path/path.dart' as p;
+import 'package:sqflite/sqflite.dart';
 
 class SettingPage extends StatefulHookConsumerWidget {
   const SettingPage({super.key});
@@ -11,6 +15,24 @@ class SettingPage extends StatefulHookConsumerWidget {
 class _SettingPageState extends ConsumerState<SettingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Text("setting");
+    return Column(
+      children: <Widget>[
+        TextButton.icon(
+          onPressed: ((() async {
+            final db = File(p.join(await getDatabasesPath(), "study.db"));
+            if (db.existsSync()) {
+              db.deleteSync();
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(const SnackBar(content: Text('削除したよ〜')));
+            } else {
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(const SnackBar(content: Text('存在しないよ〜')));
+            }
+          })),
+          icon: const Icon(Icons.delete_forever),
+          label: const Text("study.dbを削除"),
+        ),
+      ],
+    );
   }
 }
