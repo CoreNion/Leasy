f5b8aa314765bb21fe0d0ab15fd6953dfbb50b29
CoreNion <coreinion@gmail.com>
2023-01-23 13:57:41 +0900

    [page] Homeを独立したファイルに移動する

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:mimosa/pages/create.dart';
-import 'package:mimosa/pages/top.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-import 'pages/setting.dart';
-
-final titleTextProvider = StateProvider((_) => 'Leasy');
-final pageIndexProvider = StateProvider((_) => 0);
+import 'pages/home.dart';
 
 void main() async {
   sqfliteFfiInit();
@@ -42,55 +37,5 @@ class MyApp extends HookConsumerWidget {
                   useMaterial3: true),
               home: const Home(),
             )));
-  }
-}
-
-class Home extends StatefulHookConsumerWidget {
-  const Home({super.key});
-
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _HomeState();
-}
-
-class _HomeState extends ConsumerState<Home> {
-  @override
-  Widget build(BuildContext context) {
-    StateController<String> title = ref.watch(titleTextProvider.notifier);
-    StateController<int> globalIndex = ref.watch(pageIndexProvider.notifier);
-
-    return Scaffold(
-      appBar: AppBar(title: Text(title.state)),
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (index) {
-          setState(() => globalIndex.state = index);
-        },
-        destinations: const <Widget>[
-          NavigationDestination(
-            icon: Icon(Icons.home),
-            selectedIcon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.add),
-            selectedIcon: Icon(Icons.add),
-            label: '問題を作成',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.settings),
-            selectedIcon: Icon(Icons.settings),
-            label: '設定',
-          ),
-        ],
-        selectedIndex: globalIndex.state,
-      ),
-      body: IndexedStack(
-        index: globalIndex.state,
-        children: const <Widget>[
-          TopPage(),
-          CreateSubjectPage(),
-          SettingPage(),
-        ],
-      ),
-    );
   }
 }
