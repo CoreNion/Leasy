bda279172ef797c5a9e7a3afa5625ad45c9f307f
CoreNion <coreinion@gmail.com>
2023-02-03 14:33:16 +0900

    [Study] 入力問題を実装

--- lib/pages/subject/section/study.dart
+++ lib/pages/subject/section/study.dart
@@ -25,6 +25,11 @@ class _SectionStudyPageState extends ConsumerState<SectionStudyPage> {
   late MiQuestion currentMi;
   late List<MiQuestion> mis;
   bool answered = false;
+  late bool setInputQuestion;
+
+  final _formKey = GlobalKey<FormState>();
+  late String inputAnswer;
+  TextEditingController textController = TextEditingController();
 
   @override
   void initState() {
@@ -33,6 +38,7 @@ class _SectionStudyPageState extends ConsumerState<SectionStudyPage> {
     // 最初に表示する問題を設定
     mis = widget.miQuestions;
     currentMi = mis.first;
+    setInputQuestion = currentMi.isInput;
   }
 
   /// 指定された問題に表示を書き換える関数
@@ -43,7 +49,10 @@ class _SectionStudyPageState extends ConsumerState<SectionStudyPage> {
         answered = false;
         currentMi = mis[questionIndex - 1];
         currentQuestionIndex = questionIndex;
+        setInputQuestion = currentMi.isInput;
       });
+      // 入力問題の入力済みテキストを削除
+      textController.clear();
     } else if (questionIndex > mis.length) {
       // 最後の問題より上の数だったら終了するかを尋ねる
       showDialog<bool?>(
@@ -67,6 +76,136 @@ class _SectionStudyPageState extends ConsumerState<SectionStudyPage> {
         }
       });
     }
+  }
+
+  /// 選択問題の解答部分
+  Column multipleChoice() {
+    return Column(
+      children: currentMi.choices.asMap().entries.map((entry) {
+        return Expanded(
+            child: Container(
+          margin: const EdgeInsets.only(top: 5, bottom: 5),
+          width: double.infinity,
+          child: ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor: (answered
+                    ? ((currentMi.answer == entry.key + 1)
+                        ? MaterialStateProperty.all(Colors.green)
+                        : MaterialStateProperty.all(Colors.red))
+                    : null),
+                foregroundColor:
+                    answered ? MaterialStateProperty.all(Colors.white) : null),
+            onPressed: answered
+                ? null
+                : () {
+                    setState(() {
+                      answered = true;
+                    });
+
+                    if (currentMi.answer == entry.key + 1) {
+                      onCorrect(context);
+                    } else {
+                      StatusAlert.show(
+                        context,
+                        duration: const Duration(milliseconds: 1500),
+                        title: '不正解',
+                        subtitle: '正解は${currentMi.answer}番です',
+                        configuration:
+                            const IconConfiguration(icon: Icons.close),
+                        maxWidth: 260,
+                      );
+                    }
+                  },
+            child: Text(
+              "${entry.key + 1}: ${entry.value}",
+              style: const TextStyle(fontSize: 16),
+            ),
+          ),
+        ));
+      }).toList(),
+    );
+  }
+
+  /// 入力問題の解答部分
+  Column inputChoice() {
+    final correctAnswer = currentMi.choices[currentMi.answer - 1];
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Form(
+          key: _formKey,
+          child: TextFormField(
+            controller: textController,
+            decoration: const InputDecoration(
+                labelText: "解答",
+                icon: Icon(Icons.dashboard),
+                hintText: "解答を正確に入力"),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "解答を入力してください。";
+              } else {
+                inputAnswer = value;
+                return null;
+              }
+            },
+          ),
+        ),
+        Container(
+            margin: const EdgeInsets.only(top: 5, bottom: 10),
+            child: answered
+                ? Text(
+                    "正解: $correctAnswer",
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.redAccent),
+                  )
+                : null),
+        Container(
+          margin: const EdgeInsets.only(bottom: 5),
+          width: double.infinity,
+          height: 50,
+          child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  setState(() {
+                    answered = true;
+                  });
+                  if (inputAnswer == correctAnswer) {
+                    onCorrect(context);
+                  } else {
+                    StatusAlert.show(
+                      context,
+                      duration: const Duration(milliseconds: 1500),
+                      title: '不正解',
+                      subtitle: '正解は「$correctAnswer」です',
+                      configuration: const IconConfiguration(icon: Icons.close),
+                      maxWidth: 260,
+                    );
+                  }
+                }
+              },
+              child: const Text("答え合わせ")),
+        )
+      ],
+    );
+  }
+
+  // 正解時の処理
+  void onCorrect(BuildContext context) {
+    const duration = Duration(seconds: 1);
+    StatusAlert.show(
+      context,
+      duration: duration,
+      title: '正解！',
+      configuration: const IconConfiguration(icon: Icons.check_circle),
+      maxWidth: 260,
+    );
+
+    // 次の問題に進む
+    Future.delayed(duration).then((value) {
+      setQuestionUI(currentQuestionIndex + 1);
+    });
   }
 
   @override
@@ -100,75 +239,7 @@ class _SectionStudyPageState extends ConsumerState<SectionStudyPage> {
               ),
               Expanded(
                   flex: 3,
-                  child: Column(
-                    children: currentMi.choices
-                        .asMap()
-                        .entries
-                        .map((entry) => Expanded(
-                                child: Container(
-                              margin: const EdgeInsets.only(top: 5, bottom: 5),
-                              width: double.infinity,
-                              child: ElevatedButton(
-                                style: ButtonStyle(
-                                    backgroundColor: (answered
-                                        ? ((currentMi.answer == entry.key + 1)
-                                            ? MaterialStateProperty.all(
-                                                Colors.green)
-                                            : MaterialStateProperty.all(
-                                                Colors.red))
-                                        : null),
-                                    foregroundColor: answered
-                                        ? MaterialStateProperty.all(
-                                            Colors.white)
-                                        : null),
-                                onPressed: answered
-                                    ? null
-                                    : () {
-                                        setState(() {
-                                          answered = true;
-                                        });
-
-                                        if (currentMi.answer == entry.key + 1) {
-                                          const duration = Duration(seconds: 1);
-                                          StatusAlert.show(
-                                            context,
-                                            duration: duration,
-                                            title: '正解！',
-                                            configuration:
-                                                const IconConfiguration(
-                                                    icon: Icons.check_circle),
-                                            maxWidth: 260,
-                                          );
-
-                                          // 次の問題に進む
-                                          Future.delayed(duration)
-                                              .then((value) {
-                                            setQuestionUI(
-                                                currentQuestionIndex + 1);
-                                          });
-                                        } else {
-                                          StatusAlert.show(
-                                            context,
-                                            duration: const Duration(
-                                                milliseconds: 1500),
-                                            title: '不正解',
-                                            subtitle:
-                                                '正解は${currentMi.answer}番です',
-                                            configuration:
-                                                const IconConfiguration(
-                                                    icon: Icons.close),
-                                            maxWidth: 260,
-                                          );
-                                        }
-                                      },
-                                child: Text(
-                                  "${entry.key + 1}: ${entry.value}",
-                                  style: const TextStyle(fontSize: 16),
-                                ),
-                              ),
-                            )))
-                        .toList(),
-                  )),
+                  child: setInputQuestion ? inputChoice() : multipleChoice()),
             ],
           ),
         ),
