14c4e4996dc5bc5bfedc0c1ad9539a2dd46944c5
CoreNion <coreinion@gmail.com>
2023-03-03 11:50:01 +0900

    [App] 設定や動的カラーなどを読み込んだ後にスプラッシュを閉じるよう変更
    非対応のプラットフォームでも、読み込むまでは真っ黒の画面を表示 (ダークモード時でも真っ白の画面が表示され目が終わるため)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 import 'pages/home.dart';
 
 void main() async {
+  if (Platform.isIOS || Platform.isAndroid) {
+    final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+    FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  }
+
   sqfliteFfiInit();
 
   runApp(
@@ -38,89 +46,103 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-  void initState() {
-    super.initState();
-
-    SharedPreferences.getInstance().then((prefs) async {
-      final customColorKey = prefs.getBool("CustomColor");
-      if (customColorKey != null) {
-        MyApp.customColor = customColorKey;
-
-        switch (prefs.getString("ThemeMode")) {
-          case "system":
-            MyApp.themeMode = ThemeMode.system;
-            break;
-          case "dark":
-            MyApp.themeMode = ThemeMode.dark;
-            break;
-          case "light":
-            MyApp.themeMode = ThemeMode.light;
-            break;
-          default:
-        }
-
-        MyApp.seedColor = Color(prefs.getInt("SeedColor")!);
-      } else {
-        // 初期化
-        await prefs.setBool("CustomColor", false);
-        await prefs.setString("ThemeMode", "system");
-        await prefs.setInt("SeedColor", MyApp.seedColor.value);
-      }
-
-      MyApp.prefs = prefs;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     MyApp.rootSetState = setState;
 
-    return DynamicColorBuilder(builder: ((lightDynamic, darkDynamic) {
-      late ColorScheme lightScheme;
-      late ColorScheme darkScheme;
+    return FutureBuilder(
+      future: SharedPreferences.getInstance().then((prefs) async {
+        final customColorKey = prefs.getBool("CustomColor");
+        if (customColorKey != null) {
+          MyApp.customColor = customColorKey;
 
-      if (lightDynamic != null) {
-        MyApp.supportDynamicColor = true;
-      } else {
-        MyApp.supportDynamicColor = false;
-        MyApp.customColor = true;
-      }
+          switch (prefs.getString("ThemeMode")) {
+            case "system":
+              MyApp.themeMode = ThemeMode.system;
+              break;
+            case "dark":
+              MyApp.themeMode = ThemeMode.dark;
+              break;
+            case "light":
+              MyApp.themeMode = ThemeMode.light;
+              break;
+            default:
+          }
 
-      if (!MyApp.customColor) {
-        lightScheme = lightDynamic != null
-            ? lightDynamic.harmonized()
-            : ColorScheme.fromSeed(
-                seedColor: MyApp.seedColor, brightness: Brightness.light);
-        darkScheme = darkDynamic != null
-            ? darkDynamic.harmonized()
-            : ColorScheme.fromSeed(
-                seedColor: MyApp.seedColor, brightness: Brightness.dark);
-      } else {
-        lightScheme = ColorScheme.fromSeed(
-            seedColor: MyApp.seedColor, brightness: Brightness.light);
-        darkScheme = ColorScheme.fromSeed(
-            seedColor: MyApp.seedColor, brightness: Brightness.dark);
-      }
+          MyApp.seedColor = Color(prefs.getInt("SeedColor")!);
+        } else {
+          // 初期化
+          await prefs.setBool("CustomColor", false);
+          await prefs.setString("ThemeMode", "system");
+          await prefs.setInt("SeedColor", MyApp.seedColor.value);
+        }
 
-      // backgroundにDynamic Colorの色味を付ける (デフォルトだと真っ白)
-      lightScheme =
-          lightScheme.copyWith(background: lightScheme.onInverseSurface);
+        MyApp.prefs = prefs;
+        return prefs;
+      }),
+      builder:
+          (BuildContext context, AsyncSnapshot<SharedPreferences> snapshot) {
+        if (snapshot.hasData) {
+          if (Platform.isIOS || Platform.isAndroid) {
+            FlutterNativeSplash.remove();
+          }
 
-      return MaterialApp(
-        title: 'Leasy',
-        theme: ThemeData(
-            colorScheme: lightScheme,
-            useMaterial3: true,
-            scaffoldBackgroundColor: Colors.white,
-            appBarTheme: AppBarTheme.of(context)
-                .copyWith(backgroundColor: lightScheme.onInverseSurface)),
-        darkTheme: ThemeData(
-            colorScheme: darkScheme,
-            scaffoldBackgroundColor: Colors.black,
-            useMaterial3: true),
-        themeMode: MyApp.themeMode,
-        home: const Home(),
-      );
-    }));
+          return DynamicColorBuilder(builder: ((lightDynamic, darkDynamic) {
+            late ColorScheme lightScheme;
+            late ColorScheme darkScheme;
+
+            if (lightDynamic != null) {
+              MyApp.supportDynamicColor = true;
+            } else {
+              MyApp.supportDynamicColor = false;
+              MyApp.customColor = true;
+            }
+
+            if (!MyApp.customColor) {
+              lightScheme = lightDynamic != null
+                  ? lightDynamic.harmonized()
+                  : ColorScheme.fromSeed(
+                      seedColor: MyApp.seedColor, brightness: Brightness.light);
+              darkScheme = darkDynamic != null
+                  ? darkDynamic.harmonized()
+                  : ColorScheme.fromSeed(
+                      seedColor: MyApp.seedColor, brightness: Brightness.dark);
+            } else {
+              lightScheme = ColorScheme.fromSeed(
+                  seedColor: MyApp.seedColor, brightness: Brightness.light);
+              darkScheme = ColorScheme.fromSeed(
+                  seedColor: MyApp.seedColor, brightness: Brightness.dark);
+            }
+
+            // backgroundにDynamic Colorの色味を付ける (デフォルトだと真っ白)
+            lightScheme =
+                lightScheme.copyWith(background: lightScheme.onInverseSurface);
+
+            return MaterialApp(
+              title: 'Leasy',
+              theme: ThemeData(
+                  colorScheme: lightScheme,
+                  useMaterial3: true,
+                  scaffoldBackgroundColor: Colors.white,
+                  appBarTheme: AppBarTheme.of(context)
+                      .copyWith(backgroundColor: lightScheme.onInverseSurface)),
+              darkTheme: ThemeData(
+                  colorScheme: darkScheme,
+                  scaffoldBackgroundColor: Colors.black,
+                  useMaterial3: true),
+              themeMode: MyApp.themeMode,
+              home: const Home(),
+            );
+          }));
+        } else if (snapshot.connectionState != ConnectionState.done) {
+          return Container(
+            color: Colors.black,
+          );
+        } else {
+          return const Text(
+            "?",
+          );
+        }
+      },
+    );
   }
 }
